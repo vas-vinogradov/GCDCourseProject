c1cf18869bb0a93b25ac1ed5fed56b8dabb93708
vas-vinogradov <Vasiliy.Vinogradov@gmail.com>
2015-04-25 20:58:41 +0300

    Some comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,12 @@
-#SetUp
-#Reading column names from
+##Preparing Data
+
+#Reading column names from file
 col_names<-read.table("Dataset\\features.txt")$V2
+#Reading activity labels
 activities<-read.table("Dataset\\activity_labels.txt")
+
+##Test Data
+
 #Reading Test Data
 test_x_data<-read.table("Dataset\\test\\X_test.txt")
 #Setting up Feature names
@@ -13,6 +18,7 @@ s_data<-read.table("Dataset\\test\\subject_test.txt")$V1
 #Combining
 test_data<-cbind(y_data,s_data,test_x_data)
 
+##Train Data
 
 #Reading Train Data
 train_x_data<-read.table("Dataset\\train\\X_train.txt")
@@ -25,10 +31,17 @@ s_data<-read.table("Dataset\\train\\subject_train.txt")$V1
 #Combining
 train_data<-cbind(y_data,s_data,train_x_data)
 
+##Merging Data
+
 data<-rbind(test_data,train_data)
+
+##Filtering columns
 sub_data<-data[,c("y_data","s_data",colnames(data)[c(grep("std\\(\\)",colnames(data)),
                                     grep("mean\\(\\)",colnames(data)))])]
 
+##Creating new dataset
 result<-sub_data %>% group_by(y_data,s_data) %>% summarise_each(funs(mean))
+
+#Saving results
 write.table(result,file="test.txt",row.names = F) 
 
